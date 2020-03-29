50c8cce432c5557bf63d7f11876b69d9c380f98b
Env1 <bomcon123456@gmail.com>
2020-03-29 21:39:16 +0700

    update feature 1

--- hello.py
+++ hello.py
@@ -1,6 +1,2 @@
 print('Hello World!')
-<<<<<<< HEAD
-print('Update this line please')
-=======
-print('Update this lnie to another')
->>>>>>> 2a41e2af5575d9a8fbb346c3adf1c1b29b1598a2
+print('Update this line please feeffe')
